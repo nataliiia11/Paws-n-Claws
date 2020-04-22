01f53bdfdfe08efda90a23f42d60df631dbfb4f2
hanhph <46076398+Iliesevzy@users.noreply.github.com>
2020-04-22 17:40:41 +0700

    Add files via upload
    Story_001 Personal Space&Home Page

---
+++ router.js
@@ -0,0 +1,33 @@
+const httpStatus = require("http-status-codes"),
+ htmlContentType = {
+ "Content-Type": "text/html"
+ },
+ routes = { 
+ "GET": {
+//  "/contact": (req, res) => {
+//  res.writeHead(httpStatus.OK, {
+//  "Content-Type": "text/plain"
+//  })
+//  res.end("This is a contact page!")
+//  }
+ },
+ 'POST': {}
+ };
+exports.handle = (req, res) => { 
+ try {
+ if (routes[req.method][req.url]) { //tìm routes tương ứng trong routes object qua method và url
+ routes[req.method][req.url](req, res);
+ } else {
+ res.writeHead(httpStatus.NOT_FOUND, htmlContentType);
+ res.end("<h1>No such file exists</h1>");
+ }
+ } catch (ex) {
+ console.log("error: " + ex);
+ }
+};
+exports.get = (url, action) => { 
+ routes["GET"][url] = action;
+};
+exports.post = (url, action) => {
+ routes["POST"][url] = action;
+};
