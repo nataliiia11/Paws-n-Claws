b747a61064bdef26846feca6d3112ef4d18482f3
nataliiia11 <azarnykhna@gmail.com>
2020-04-22 21:06:42 +0200

    Added html file with layout for Personal Page. It might be seen in the address "http://localhost:3000/personal.html"

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ const router = require("./router"),
 plainTextContentType = {
     "Content-Type": "text/plain"
     },
+
     htmlContentType = {
     "Content-Type": "text/html"
     },
+
     customReadFile = (file, res) => { 
     fs.readFile(`./${file}`, (errors, data) => {
     if (errors) {
@@ -20,18 +22,22 @@ plainTextContentType = {
     res.end(data);
     });
     };
+
    router.get("/post", (req, res) => { 
     res.writeHead(httpStatusCodes.OK, plainTextContentType);
     res.end("INDEX");
    });
+
    router.get("/personal.html", (req, res) => {
     res.writeHead(httpStatusCodes.OK, htmlContentType);
     customReadFile("views/personal.html", res);
    });
+
    router.post("/", (req, res) => {
     res.writeHead(httpStatusCodes.OK, plainTextContentType);
     res.end("POSTED");
    });
+   
    http.createServer(router.handle).listen(3000); 
    console.log(`The server is listening on port number:
    ${port}`);
