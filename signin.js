9a72595ebbf94676ee2660bc2a71f808e19b3cc9
Diro Baloska <dirobaloska@outlook.com>
2020-05-01 20:11:33 +0200

    mongoose test

---
+++ signin.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    "mongodb://localhost:27017/PawsAndClaws",
+    {useNewUrlParser: true});
+
+const database = mongoose.connection;
+
+database.once("open", () => {
+    console.log("Connected to MongoDB using MOngoose");
+    
+
+    
+})
+const PersonSchema = mongoose.Schema({
+    name : String,
+    email : String
+});
+
+const Person = mongoose.model("Person", PersonSchema);
+
+var newPerson = new Person({
+    name : "Diro Baloska",
+    email : "dirobaloska@outlook.com"
+})
+
+newPerson.save((error, savedDocument) => {
+    if (error) throw error;
+    console.log(savedDocument);
+})
