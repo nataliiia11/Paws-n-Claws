385a2840ce095efba981cfd51de8b5bbbd3c7405
Diro Baloska <dirobaloska@outlook.com>
2020-05-02 20:12:24 +0200

    index.html into index.ejs

--- main.js
+++ main.js
@@ -2,8 +2,13 @@
 const port = 3000;
 const express=require('express');
 const ejs= require('ejs')
+const path = require('path');
 const bodyParser = require('body-parser')
 const app= new express();
+app.use("/public", express.static(path.join(__dirname,"public")));
+app.get('/', (req,res) => {
+    res.render("index");
+})
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
