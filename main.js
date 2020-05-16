e41b9d1917dd8e0cfdbcc2427b3e4c6d91e431d8
hanh <hhanh.phan@outlook.com>
2020-05-16 10:22:40 +0700

    Add procfile,update gitignore

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ app.post('/upload', upload.single('photo'), (req, res) => {
 
    
       
-app.listen(3000,()=>
+app.listen(process.env.PORT||3000,()=>
 	console.log(`Listening on Port ${port}`));
 
 
