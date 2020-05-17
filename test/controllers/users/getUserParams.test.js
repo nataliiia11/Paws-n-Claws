24dad6e0b983a0904c88fef0d95d7435ec1cba57
hanh <hhanh.phan@outlook.com>
2020-05-17 21:39:49 +0700

    Fix hanging test process

--- test/controllers/users/getUserParams.test.js
+++ test/controllers/users/getUserParams.test.js
@@ -4,17 +4,17 @@ const userData = { username: 'sample', email: 'sample', password:'123' };
 
 describe('User Model Test', () => {
 
-    beforeAll(async (done) => {
+    beforeAll(async () => {
         await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
             if (err) {
                 console.error(err);
                 process.exit(1);
             }
-            done();
+            
         });
     });
   
-    it('create & save user successfully', async (done) => {
+    it('create & save user successfully', async () => {
         const validUser = new UserModel(userData);
         const savedUser = await validUser.save();
         // Object Id should be defined when successfully saved to MongoDB.
@@ -22,23 +22,23 @@ describe('User Model Test', () => {
         expect(savedUser.username).toBe(userData.username);
         expect(savedUser.email).toBe(userData.email);
         expect(savedUser.password).toBe(userData.password);
-        done();
+        
     });
 
     // Test Schema is working!!!
-    // You shouldn't be able to add in any field that isn't defined in the schema
-    it('insert user successfully, but the field does not defined in schema should be undefined', async (done) => {
+    // Shouldn't be able to add in any field that isn't defined in the schema
+    it('insert user successfully, but the field does not defined in schema should be undefined', async () => {
         const userWithInvalidField = new UserModel({ username: 'sample2', email:'sample@gmail.com',password:'1234',add:'str54' });
         const savedUserWithInvalidField = await userWithInvalidField.save();
         expect(savedUserWithInvalidField._id).toBeDefined();
         expect(savedUserWithInvalidField.add).toBeUndefined();
-        done();
+        
     });
 
     // Test Validation is working!!!
-    // It should us told us the errors in on gender field.
-    it('create user without required field should failed', async (done) => {
-        const userWithoutRequiredField = new UserModel({ username: 'TekLoon' });
+    // Should show errors in mail field.
+    it('create user without required field should failed', async () => {
+        const userWithoutRequiredField = new UserModel({ username: 'sample3' });
         let err;
         try {
             const savedUserWithoutRequiredField = await userWithoutRequiredField.save();
@@ -48,7 +48,7 @@ describe('User Model Test', () => {
         }
         expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
         expect(err.errors.email).toBeDefined();
-        done();
+        
     });
 
     
