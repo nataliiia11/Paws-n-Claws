7fdcf3b184761951dd2073ccfe4311c9f19d79df
Fhuu <dirobaloska@outlook.com>
2020-05-17 09:40:58 +0200

    new commit

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict'
+const express=require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const app= new express();
+const multer = require('multer');
+const upload = multer({dest: __dirname + '/uploads/images'});
+const userRouter = require('./Router/userRouter');
+const postController = require('./Controller/postController');
+const errorController = require('./Controller/errorController');
+const userController = require('./Controller/UserController');
+
+
+app.use('/public', express.static(path.join(__dirname,'public')));
+app.use(morgan(':method:url:status*:response-time ms'))
+app.use('/user', userRouter);
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.set('port', port)
+
+//const posts=[];
+app.get('/newsfeed',postController.getAllPostsNewsfeed);
+app.get('/:page',postController.getAllPostsPersonal);
+//res.redirect('/personal/'+definedPage,{newPost:posts,page:'personal'});
+app.get('/personal',postController.getAllPostsPersonal);
+
+
+app.get('/',(req,res)=>{
+	res.render('index');
+});
+
+
+app.post('/:page/delete',postController.deletePost);
+
+
+
+app.post('/:page',postController.savePost);
+
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+	if(req.file) {
+		res.json(req.file);
+	}
+	else throw 'error';
+});
+
+module.exports = app
--- main.js
+++ main.js
@@ -1,74 +1,23 @@
 
-const port = 3000;
 
-const express=require('express');
-const ejs= require('ejs')
-const path = require('path');
-const bodyParser = require('body-parser')
-const app= new express();
-const multer = require('multer');
-const upload = multer({dest: __dirname + '/uploads/images'});
-const userRouter = require("./Router/userRouter")
-postController = require("./Controller/postController"),
-errorController = require("./Controller/errorController"),
+'use strict'
 
-app.use("/public", express.static(path.join(__dirname,"public")));
-app.use('/user', userRouter)
-
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-
-const posts=[];
-app.get("/personal",postController.getAllPostsPersonal);
-app.get('/newsfeed',postController.getAllPostsNewsfeed);
-// app.get('/:page',(req,res)=>{
-//     const definedPage=req.params.page
-
-//     res.render(definedPage,{newPost:posts,page:definedPage});
-// })
-app.get('/',(req,res)=>{
-    res.render('index')
-})
-
-app.post('/personal/delete',postController.deletePost)
-
-
-//old post to personal
-// app.post('/personal',(req,res)=>{
-// post=req.body.newPost;
-// posts.push(post);
-// res.render('personal',{newPost:posts,page:"personal"});
-
-// }
-//)
-app.post('/personal',postController.savePost)
-
-// app.post('/newsfeed',(req,res)=>{
-//     post=req.body.newPost;
-//     posts.push(post);
-//     res.render('newsfeed',{newPost:posts,page:"newsfeed"});
-    
-//     }
-//     )
-    app.post('/upload', upload.single('photo'), (req, res) => {
-        if(req.file) {
-            res.json(req.file);
-        }
-            else throw 'error'
-    });
-
-   
+const app = require('./app')
+const mongoose = require('mongoose');  
       
-app.listen(3000,()=>
-console.log("Listening on Port 3000"))
+
 
 
 //mongoose implementation
-const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/PawsAndClaws', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin-hanh:hanh@cluster1-yhbkr.mongodb.net/PawsAndClaws', {useNewUrlParser: true, useUnifiedTopology: true});
 var database = mongoose.connection;
-database.on("error", console.error.bind(console, 'Connection error'));
-database.once("open", () => {
-    console.log("Connection to database Paws And Claws succesfull.")
-})
+database.on('error', console.error.bind(console, 'Connection error'));
+database.once('open', () => {
+	console.log('Connection to database Paws And Claws succesfull.');
+});
+
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Server running at http://localhost:${app.get('port')}`)
+  })
