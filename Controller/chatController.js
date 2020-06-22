b0b6c196dda487c20d14fecedc1df3893d74c7e5
hanh <hhanh.phan@outlook.com>
2020-06-22 16:06:06 +0700

    Real time chat

--- Controller/chatController.js
+++ Controller/chatController.js
@@ -1,51 +1,7 @@
 
 
 const Message = require('../model/Message');
-// module.exports = io => {
-  
-        
-	// io.on('connection', (socket) => {
-	// 	console.log('new connection');
-	// 	socket.on('chat message', (msg) => {
-	// 		io.emit('chat message', msg);
-	// 	});
-	// });
-      // };
-	//   "use strict";
 
-	//   const Message = require("../model/Message");
-	  
-	//   module.exports = io => {
-	// 	io.on("connection", client => {
-	// 	  console.log("new connection");
-		  
-	  
-	// 	  Message.find({})
-	// 		.sort({ createdAt: -1 })
-	// 		.limit(10)
-	// 		.then(messages => {
-	// 		  client.emit("load all messages", messages.reverse());
-	// 		});
-	  
-	// 	  client.on("disconnect", () => {
-	// 		console.log("user disconnected");
-	// 	  });
-	  
-	// 	  client.on("message", data => {
-	// 		let messageAttributes = {
-	// 			content: data.content,
-	// 			userName: data.userName,
-	// 			user: data.userId
-	// 		  },
-	// 		  m = new Message(messageAttributes);
-	// 		m.save()
-	// 		  .then(() => {
-	// 			io.emit("message", messageAttributes);
-	// 		  })
-	// 		  .catch(error => console.log(`error: ${error.message}`));
-	// 	  });
-	// 	});
-	//   };
 module.exports = io => {
 users = [];
 io.on('connection', function(socket) {
